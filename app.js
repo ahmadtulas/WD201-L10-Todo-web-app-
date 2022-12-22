4d3f7dd738b75387670ed9a5b192bc345a040815
Ahmad Jamal <ahmadjamalcs@gmail.com>
2022-12-22 21:46:55 +0530

    level 9 requirment added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const {request, response} = require('express');
 const express = require('express');
 const app = express();
-const csrf = require('csurf');
+const csrf = require('tiny-csrf');
 
 const {Todo} = require('./models');
 const bodyParser = require('body-parser');
@@ -13,7 +13,7 @@ const path = require('path');
 
 app.use(bodyParser.json());
 app.use(cookieParser('ssh!!!! some secret string'));
-app.use(csrf({cookie: true}));
+app.use(csrf('this_should_be_32_character_long', ['POST', 'PUT', 'DELETE']));
 
 
 // seting the ejs is the engine
@@ -24,9 +24,10 @@ app.get('/', async (request, response)=>{
   const overdue = await Todo.overdue();
   const dueToday = await Todo.dueToday();
   const dueLater = await Todo.dueLater();
+  const completedItems = await Todo.completedItems();
   if (request.accepts('html')) {
     response.render('index', {
-      allTodos, overdue, dueToday, dueLater,
+      allTodos, overdue, dueToday, dueLater, completedItems,
       csrfToken: request.csrfToken(),
     });
   } else {
@@ -73,6 +74,15 @@ app.put('/todos/:id/markAsCompleted', async (request, response)=>{
 // app.delete('/todos/:id', (request, response)=>{
 //   console.log('Delete a todo by id', request.params.id);
 // });
+app.put('/todos/:id', async (request, response) => {
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const upTodo = await todo.setCompletionStatus(request.body.completed);
+    return response.json(upTodo);
+  } catch (error) {
+    return response.status(422).json(error);
+  }
+});
 
 app.delete('/todos/:id', async function(request, response) {
   console.log('We have to delete a Todo with ID: ', request.params.id);
